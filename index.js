eaf31e9e77d27b34c9c73fb514bea716010bf80f
gri-espindola <griespindola@hotmail.com>
2022-01-07 21:48:01 -0300

    trabajando funcionalidad de buscador, revisar con clase 87

--- index.js
+++ index.js
@@ -26,4 +26,33 @@ const crearTarjetasGift = (data) =>{
   gift.innerHTML = htmlGift
 }
 
+//LLAMAR AL BOTON BUSCAR PARA DAR ESTA ACCION
+
+const buscador = document.querySelector("#buscador");
+
+buscador.addEventListener("click", (e) =>{
+//a desarrollar idea
+
+  return buscarGift
+})
+
+fetch (`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=`)
+.then((res) => res.json())
+.then((data) => {
+  console.log (data)
+ buscarGift (data.data) 
+})
+
+const buscarGift = (data) =>{
+  const giftBuscado = document.getElementById("gift-buscado")
+  const GiftEncontrado = data.reduce ((acc,curr,index) =>{  
+    return acc + `
+  <article class="gift-card">
+  <img class="img-gift" src="${data[index].images.fixed_height.url}" alt="Resultado de la busqueda">
+  </article>
+  `
+  },"")
+
+  giftBuscado.innerHTML = GiftEncontrado
+}
 
