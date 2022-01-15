42ec059c91a994d94e1ef07a820c72616c26bcf0
gri-espindola <griespindola@hotmail.com>
2022-01-14 23:05:47 -0300

    Buscador estilado, función buscar ok, haciendo andar seccion gatitos

--- index.js
+++ index.js
@@ -1,19 +1,89 @@
 const container = document.getElementById("container");
-const formBusquedaGift = document.getElementById ("form-busqueda-gift")
+const formBusquedaGif = document.getElementById ("form-busqueda-gif")
 const API_KEY = "766uz6yWVGCfNJT8GXBCq9q7N4q96tms";
 const inputBuscador = document.getElementById ("input-buscador");
 const buscador = document.getElementById ("buscador");
+let valueInputBusqueda = "";
+const giftBuscado = document.getElementById("gif-buscado");
+const seccionGatitos = document.getElementById("seccion-gatitos");
+ 
+
+
+//BUSCADOR
+
+const buscarGif = () =>{
+  fetch (`https://api.giphy.com/v1/gifs/search?api_key=766uz6yWVGCfNJT8GXBCq9q7N4q96tms&q=${valueInputBusqueda}`)
+  .then((res) => res.json())
+  .then((data) => {
+  giftBuscado.innerHTML = gifAHtml (data.data)
+})
+}
+ 
+ const gifAHtml = (data) =>{
+  
+  const reduceAData = data.reduce ((acc,curr) =>{ 
+
+    return acc + `
+    <article class="gift-card">
+    <img class="img-gift" src="${curr.images.fixed_height.url}" alt="Resultado de la busqueda">
+    </article>
+    `
+  },"")
+    return reduceAData;
+  }
+
+formBusquedaGif.onsubmit = (e) =>{
+  e.preventDefault()
+  valueInputBusqueda = inputBuscador.value; 
+  buscarGif();
+} 
+
+//Sección gatitos
+
+const ferchAGatitos = () =>{
+  fetch (`https://api.giphy.com/v1/gifs/search?api_key=766uz6yWVGCfNJT8GXBCq9q7N4q96tms&q=gatos`)
+  .then((res) => res.json())
+  .then((data) => {
+  console.log (data)
+  
+  seccionGatitos.innerHTML = gatitosAHtml (data.data)
+})
+}
+const gatitosAHtml = (data) =>{
+  
+  const reduceADataGatitos = data.reduce ((acc,curr) =>{ 
+
+    return acc + `
+    <article class="gift-card">
+    <img class="img-gift" src="${curr.images.fixed_height.url}" alt="Resultado de la busqueda">
+    </article>
+    `
+  },"")
+    return reduceADataGatitos;
+  }
+
+  const llamandoSeccionGatitos = () =>{
+    seccionGatitos.addEventListener ('click' = ()=>{
+      seccionPerrito
+    })
+  }
 
 
 
- fetch (`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=gift`)
+
+
+
+
+
+
+/*  fetch (`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=gift`)
    .then((res) => res.json())
    .then((data) => {
      console.log (data)
     crearTarjetasGift (data.data) 
 })
 
-const crearTarjetasGift = (data) =>{
+const crearTarjetasGif = (data) =>{
   const gift = document.getElementById("gift")
   const htmlGift = data.reduce ((acc,curr,index) =>{  
     return acc + `
@@ -24,35 +94,4 @@ const crearTarjetasGift = (data) =>{
   },"")
 
   gift.innerHTML = htmlGift
-}
-
-//LLAMAR AL BOTON BUSCAR PARA DAR ESTA ACCION
-
-const buscador = document.querySelector("#buscador");
-
-buscador.addEventListener("click", (e) =>{
-//a desarrollar idea
-
-  return buscarGift
-})
-
-fetch (`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=`)
-.then((res) => res.json())
-.then((data) => {
-  console.log (data)
- buscarGift (data.data) 
-})
-
-const buscarGift = (data) =>{
-  const giftBuscado = document.getElementById("gift-buscado")
-  const GiftEncontrado = data.reduce ((acc,curr,index) =>{  
-    return acc + `
-  <article class="gift-card">
-  <img class="img-gift" src="${data[index].images.fixed_height.url}" alt="Resultado de la busqueda">
-  </article>
-  `
-  },"")
-
-  giftBuscado.innerHTML = GiftEncontrado
-}
-
+} */
