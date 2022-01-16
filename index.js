d48228a55f881b66c3ed11a0daa22ebd009397e9
gri-espindola <griespindola@hotmail.com>
2022-01-16 18:02:45 -0300

    haciendo funciones de busquedas populares

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const inputBuscador = document.getElementById ("input-buscador");
 const buscador = document.getElementById ("buscador");
 let valueInputBusqueda = "";
 const giftBuscado = document.getElementById("gif-buscado");
-const seccionGatitos = document.getElementById("seccion-gatitos");
- 
+const linkGatitos = document.getElementById("link-gatitos");
+const linkPerritos = document.getElementById ("link-perritos");
+const linkSpiderMan = document.getElementById ("link-spiderman");
+const linkAvengers = document.getElementById ("link-avengers");
+const linkAmor = document.getElementById ("link-amor");
+const cardGatitos= document.getElementById ("card-gatitos");
+const cardPerritos = document.getElementById("card- perritos");
+const cardSpiderMan = document.getElementById("card-spiderman");
+const cardAvengers = document.getElementById("card-avengers");
+const cardAmor = document.getElementById("card-amor");
+
 
 
 //BUSCADOR
@@ -38,6 +47,14 @@ formBusquedaGif.onsubmit = (e) =>{
   buscarGif();
 } 
 
+//Sección Busquedas populares
+
+cardGatitos.style.display = "none";
+cardPerritos.style.display = "none";
+cardSpiderMan.style.display = "none";
+cardAvengers.style.display = "none";
+cardAmor.style.display = "none";
+
 //Sección gatitos
 
 const ferchAGatitos = () =>{
@@ -46,7 +63,7 @@ const ferchAGatitos = () =>{
   .then((data) => {
   console.log (data)
   
-  seccionGatitos.innerHTML = gatitosAHtml (data.data)
+  linkGatitos.innerHTML = gatitosAHtml (data.data)
 })
 }
 const gatitosAHtml = (data) =>{
@@ -54,7 +71,7 @@ const gatitosAHtml = (data) =>{
   const reduceADataGatitos = data.reduce ((acc,curr) =>{ 
 
     return acc + `
-    <article class="gift-card">
+    <article class="card-gatitos">
     <img class="img-gift" src="${curr.images.fixed_height.url}" alt="Resultado de la busqueda">
     </article>
     `
@@ -62,14 +79,154 @@ const gatitosAHtml = (data) =>{
     return reduceADataGatitos;
   }
 
-  const llamandoSeccionGatitos = () =>{
-    seccionGatitos.addEventListener ('click' = ()=>{
-      seccionPerrito
+  const llamandoBusquedaGatitos = () =>{
+    linkGatitos.addEventListener ('click', ()=>{
+      cardGatitos.style.display = "flex";
+      cardPerritos.style.display = "none";
+      cardSpiderMan.style.display = "none";
+      cardAvengers.style.display = "none";
+      cardAmor.style.display = "none";
     })
   }
 
+  //Secciòn perritos
 
+  const ferchAPerritos = () =>{
+    fetch (`https://api.giphy.com/v1/gifs/search?api_key=766uz6yWVGCfNJT8GXBCq9q7N4q96tms&q=perros`)
+    .then((res) => res.json())
+    .then((data) => {
+    console.log (data)
+    
+    linkPerritos.innerHTML = perritosAHtml (data.data)
+  })
+  }
+  const perritosAHtml = (data) =>{
+    
+    const reduceADataPerritos = data.reduce ((acc,curr) =>{ 
+  
+      return acc + `
+      <article class="card-perritos">
+      <img class="img-gift" src="${curr.images.fixed_height.url}" alt="Resultado de la busqueda">
+      </article>
+      `
+    },"")
+      return reduceADataPerritos;
+    }
+  
+    const llamandoBusquedaPerritos = () =>{
+      linkPerritos.addEventListener ('click', ()=>{
+        cardGatitos.style.display = "none";
+        cardPerritos.style.display = "flex";
+        cardSpiderMan.style.display = "none";
+        cardAvengers.style.display = "none";
+        cardAmor.style.display = "none";
+      })
+    }
 
+    //Secciòn Spider Man
+
+    const ferchASpiderMan = () =>{
+      fetch (`https://api.giphy.com/v1/gifs/search?api_key=766uz6yWVGCfNJT8GXBCq9q7N4q96tms&q=spiderman`)
+      .then((res) => res.json())
+      .then((data) => {
+      console.log (data)
+      
+      linkSpiderMan.innerHTML = SpiderManAHtml (data.data)
+    })
+    }
+    const SpiderManAHtml = (data) =>{
+      
+      const reduceADataASpiderMan = data.reduce ((acc,curr) =>{ 
+    
+        return acc + `
+        <article class="card-spiderman">
+        <img class="img-gift" src="${curr.images.fixed_height.url}" alt="Resultado de la busqueda">
+        </article>
+        `
+      },"")
+        return reduceADataASpiderMan;
+      }
+    
+      const llamandoBusquedaSpiderMan = () =>{
+        linkSpiderMan.addEventListener ('click', ()=>{
+          cardGatitos.style.display = "none";
+          cardPerritos.style.display = "none";
+          cardSpiderMan.style.display = "flex";
+          cardAvengers.style.display = "none";
+          cardAmor.style.display = "none";
+        })
+      }
+    
+
+  //Secciòn Avengers
+
+  const ferchAAvengers = () =>{
+    fetch (`https://api.giphy.com/v1/gifs/search?api_key=766uz6yWVGCfNJT8GXBCq9q7N4q96tms&q=avengers`)
+    .then((res) => res.json())
+    .then((data) => {
+    console.log (data)
+    
+    linkAvengers.innerHTML = avengersAHtml (data.data)
+  })
+  }
+  const AvengersAHtml = (data) =>{
+    
+    const reduceADataAvengers = data.reduce ((acc,curr) =>{ 
+  
+      return acc + `
+      <article class="card-avengers">
+      <img class="img-gift" src="${curr.images.fixed_height.url}" alt="Resultado de la busqueda">
+      </article>
+      `
+    },"")
+      return reduceADataAvengers;
+    }
+  
+    const llamandoBusquedaAvengers = () =>{
+      linkAvengers.addEventListener ('click', ()=>{
+        cardGatitos.style.display = "none";
+        cardPerritos.style.display = "none";
+        cardSpiderMan.style.display = "none";
+        cardAvengers.style.display = "flex";
+        cardAmor.style.display = "none";
+      })
+    }
+  
+
+  //Secciòn Amor
+
+  const ferchAAmor = () =>{
+    fetch (`https://api.giphy.com/v1/gifs/search?api_key=766uz6yWVGCfNJT8GXBCq9q7N4q96tms&q=gatos`)
+    .then((res) => res.json())
+    .then((data) => {
+    console.log (data)
+    
+    linkAmor.innerHTML = amorAHtml (data.data)
+  })
+  }
+  const amorAHtml = (data) =>{
+    
+    const reduceADataAmor = data.reduce ((acc,curr) =>{ 
+  
+      return acc + `
+      <article class="card-amor">
+      <img class="img-gift" src="${curr.images.fixed_height.url}" alt="Resultado de la busqueda">
+      </article>
+      `
+    },"")
+      return reduceADataAmor;
+    }
+  
+    const llamandoBusquedaAmor = () =>{
+      linkAmor.addEventListener ('click', ()=>{
+        cardGatitos.style.display = "none";
+        cardPerritos.style.display = "none";
+        cardSpiderMan.style.display = "none";
+        cardAvengers.style.display = "none";
+        cardAmor.style.display = "flex";
+      })
+    }
+  
 
 
 
